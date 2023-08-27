4937e7839ac35f6f6c00792b49e055a84df28a8c
Ryan <git@hexa.mozmail.com>
2023-08-26 20:20:10 -0400

    fix script

--- gh-build.sh
+++ gh-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curl -L https://fly.io/install.sh | sh
 
+FLY_EXEC=$HOME/.fly/bin/flyctl
+
 curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt --output blocklist.txt
 
-fly deploy .
+${FLY_EXEC} deploy .
